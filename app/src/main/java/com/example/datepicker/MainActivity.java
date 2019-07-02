fe4326e2c5214c8d6d7513c627b1356eb6847314
BLACKCAT25896 <joykamrujjaman59@gmail.com>
2019-07-03 03:02:27 +0600

    TimePicker

--- app/src/main/java/com/example/datepicker/MainActivity.java
+++ app/src/main/java/com/example/datepicker/MainActivity.java
@@ -4,18 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import java.text.DateFormat;
 import java.util.Calendar;
 
-public class MainActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
-    TextView date;
-    Button datePicker;
+public class MainActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener , TimePickerDialog.OnTimeSetListener {
+    TextView date, time;
+    Button datePicker, timePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,11 +32,23 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
                 datePicker.show(getSupportFragmentManager(),"date picker");
             }
         });
+
+
+        timePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogFragment timePicker = new TimePickerFragment();
+                timePicker.show(getSupportFragmentManager(),"time picker");
+            }
+        });
+
     }
 
     private void init() {
         date = findViewById(R.id.dateTV);
+        time = findViewById(R.id.timeTV);
         datePicker = findViewById(R.id.datePickerBtn);
+        timePicker = findViewById(R.id.timePickerBtn);
 
     }
 
@@ -48,6 +62,16 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
         String currentDateString = DateFormat.getDateInstance(DateFormat.FULL).format(calendar.getTime());
         date.setText(currentDateString);
 
+
+    }
+
+    @Override
+    public void onTimeSet(TimePicker timePicker, int hour, int minute) {
+
+//        Calendar calendar = Calendar.getInstance();
+//        calendar.set(Calendar.HOUR_OF_DAY,hour);
+//        calendar.set(Calendar.MINUTE, minute);
+        time.setText("Hours :" + hour + "Minutes: " + minute );
 
     }
 }
